cee45eb2e42376df1752f317f9b608eed1899e20
Ezequiel <eze.olivero96@gmail.com>
2023-10-25 15:34:11 -0300

    user menu and log in button style

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,6 +6,8 @@ import { getServerSession } from 'next-auth';
 import { options } from '@/app/api/auth/[...nextauth]/options';
 // Ui
 import { unbounded } from '@/app/fonts';
+import UserMenu from './UserMenu/UserMenu';
+import Link from 'next/link';
 
 const Header = async () => {
   const session = await getServerSession(options);
@@ -14,25 +16,26 @@ const Header = async () => {
   return (
     <header className='w-full bg-slate-800 px-6 py-8'>
       <nav className='container mx-auto flex items-center justify-between'>
-        <div>
+        <Link href={'/'}>
           <h1
             className={`text-4xl font-extrabold uppercase tracking-wider ${unbounded.className}`}
           >
             DB Forge
           </h1>
-        </div>
-        <ul className='flex space-x-6'>
+        </Link>
+        <ul className='flex items-center gap-10'>
           {session ? (
             <>
               <HeaderLink href='/dashboard'>Dashboard</HeaderLink>
-              <HeaderLink href='/api/auth/signout?callbackUrl=/'>
-                Sign out
-              </HeaderLink>
+              <UserMenu user={session.user} />
             </>
           ) : (
-            <HeaderLink href='/api/auth/signin?callbackUrl=/'>
+            <Link
+              href='/api/auth/signin?callbackUrl=/'
+              className='rounded border border-red-500 bg-red-700 px-4 py-2 font-semibold text-white hover:border-transparent hover:bg-white hover:text-red-700'
+            >
               Log In
-            </HeaderLink>
+            </Link>
           )}
         </ul>
       </nav>
