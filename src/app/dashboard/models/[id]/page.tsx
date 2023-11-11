11fe23bb1bd10fe999229fa9751ba7cba4315ef0
Ezequiel <eze.olivero96@gmail.com>
2023-11-10 21:25:36 -0300

    layouts

--- src/app/dashboard/models/[id]/page.tsx
+++ src/app/dashboard/models/[id]/page.tsx
@@ -27,23 +27,19 @@ export default function ModelDetailPage({
   }, [params.id]);
 
   return (
-    <div>
-      <div className='mx-auto max-w-7xl'>
-        <div className='mx-2 bg-gray-800 py-5'>
-          <div className='px-4 sm:px-6 lg:px-8'>
-            <ModelDetailHeader modelName={modelData?.modelName || ''} />
-            <LoadingErrorSection loading={loading} error={!modelData} />
-            {modelData && (
-              <div className='mt-8 flow-root'>
-                <div className='-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8'>
-                  <div className='inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8'>
-                    <ModelDetailTable fields={modelData.fields} />
-                  </div>
-                </div>
+    <div className='mx-2 bg-gray-800 py-5'>
+      <div className='px-4 sm:px-6 lg:px-8'>
+        <ModelDetailHeader modelName={modelData?.modelName || ''} />
+        <LoadingErrorSection loading={loading} error={!modelData} />
+        {modelData && (
+          <div className='mt-8 flow-root'>
+            <div className='-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8'>
+              <div className='inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8'>
+                <ModelDetailTable fields={modelData.fields} />
               </div>
-            )}
+            </div>
           </div>
-        </div>
+        )}
       </div>
     </div>
   );
---
+++ src/app/dashboard/models/layout.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ModelsLayout = ({ children }) => {
+  return <div className='mx-auto w-1/2 space-y-4'>{children}</div>;
+};
+
+export default ModelsLayout;
